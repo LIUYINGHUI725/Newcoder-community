77532df721e46c536963ee2e4549575399e8afff
LIUYINGHUI <iuyinghui725@gmail.com>
2022-06-29 18:08:38 +0800

    upload header & update password

--- src/main/java/com/newcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/newcoder/community/config/WebMvcConfig.java
@@ -1,6 +1,7 @@
 package com.newcoder.community.config;
 
 import com.newcoder.community.controller.interceptor.AlphaInterceptor;
+import com.newcoder.community.controller.interceptor.LoginRequiredInterceptor;
 import com.newcoder.community.controller.interceptor.LoginTicketInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +17,20 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
 
+    @Autowired
+    private LoginRequiredInterceptor loginRequiredInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        //WebMvcConfigurer.super.addInterceptors(registry);
+        //WebMvcConfigure.super.addInterceptors(registry);
         registry.addInterceptor(alphaInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg")
                 .addPathPatterns("/register", "/login");
 
         registry.addInterceptor(loginTicketInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
+        registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 
